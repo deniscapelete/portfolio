b32d23e873a94009f63542f183a14f625e1566f2
Denis <capelete.denis@hotmail.com>
2024-02-24 21:00:21 -0300

    Creating header, footer and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
 
 
   return (
     <>
-      <h1>Olá Mundo!</h1>
+      <Header />
+      <section className='container'>
+        <div className='apresentacao'>
+          <p>Olá, sou <br />
+            <span>Denis Capelete</span> <br />
+            Dev Full Stack
+          </p>
+          <button className='btn btn-red'>
+            Saiba mais
+          </button>
+        </div>
+        <figure>
+          <img className='img-home' src="/developer-red.svg" alt="Imagem de home" />
+        </figure>
+
+      </section>
+      <Footer />
     </>
   )
 }
