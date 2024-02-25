62ca8e4b27dd4c08555bcd4bcfb76f8dd49f4587
Denis <capelete.denis@hotmail.com>
2024-02-25 17:00:10 -0300

    making routes functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import Home from './pages/Home/'
+import AppRoutes from './routes'
 
 function App() {
 
   return (
-    <Home />
+    <AppRoutes />
   )
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
@@ -11,9 +12,9 @@ function Home() {
             <span>Denis Capelete</span> <br />
             Dev Full Stack
           </p>
-          <button className='btn btn-red'>
+          <Link to="/sobre" className='btn btn-red'>
             Saiba mais
-          </button>
+          </Link>
         </div>
         <figure>
           <img className='img-home' src="/developer-red.svg" alt="Imagem de home" />
