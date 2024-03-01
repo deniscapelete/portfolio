6b87288a9447490870a1ec396c968fb66309ccbb
Denis <capelete.denis@hotmail.com>
2024-03-01 16:25:14 -0300

    Nested routes in React JS with react router dom

--- src/pages/Contatos/index.jsx
+++ src/pages/Contatos/index.jsx
@@ -1,6 +1,3 @@
-import Header from "../../components/Header"
-import Footer from "../../components/Footer"
-import Container from "../../components/Container"
 import styles from "./Contatos.module.css"
 import { GoMail } from "react-icons/go"
 import { FaInstagram } from "react-icons/fa";
@@ -11,33 +8,29 @@ import { SlSocialLinkedin } from "react-icons/sl";
 function Contatos() {
   return (
     <>
-      <Header />
-      <Container>
-        <section className={styles.contatos}>
-          <h2>Contatos</h2>
-          <h3>Ente em contato</h3>
-          <p>Para que possamos conversar mais sobre.</p>
+      <section className={styles.contatos}>
+        <h2>Contatos</h2>
+        <h3>Ente em contato</h3>
+        <p>Para que possamos conversar mais sobre.</p>
 
-          <div className={styles.icones}>
-            <a href="mailto:capelete.denis@gmail.com" target="_blank" rel="noopener noreferrer">
-              <GoMail className={styles.icone} />
-            </a>
-            <a href="https://www.instagram.com/denis_ce5" target="_blank" rel="noopener noreferrer">
-              <FaInstagram className={styles.icone} />
-            </a>
-            <a href="https://github.com/deniscapelete" target="_blank" rel="noopener noreferrer">
-              <LuGithub className={styles.icone} />
-            </a>
-            <a href="https://gitlab.com/denis_ce5" target="_blank" rel="noopener noreferrer">
-              <PiGitlabLogoLight className={styles.icone} />
-            </a>
-            <a href="https://www.linkedin.com/in/denis-capelete-evangelista-902539233/" target="_blank" rel="noopener noreferrer">
-              <SlSocialLinkedin className={styles.icone} />
-            </a>
-          </div>
-        </section>
-      </Container>
-      <Footer />
+        <div className={styles.icones}>
+          <a href="mailto:capelete.denis@gmail.com" target="_blank" rel="noopener noreferrer">
+            <GoMail className={styles.icone} />
+          </a>
+          <a href="https://www.instagram.com/denis_ce5" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className={styles.icone} />
+          </a>
+          <a href="https://github.com/deniscapelete" target="_blank" rel="noopener noreferrer">
+            <LuGithub className={styles.icone} />
+          </a>
+          <a href="https://gitlab.com/denis_ce5" target="_blank" rel="noopener noreferrer">
+            <PiGitlabLogoLight className={styles.icone} />
+          </a>
+          <a href="https://www.linkedin.com/in/denis-capelete-evangelista-902539233/" target="_blank" rel="noopener noreferrer">
+            <SlSocialLinkedin className={styles.icone} />
+          </a>
+        </div>
+      </section>
     </>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,23 @@
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
 import { Link } from 'react-router-dom'
-import Container from '../../components/Container'
 import styles from './Home.module.css'
 
 function Home() {
   return (
     <>
-      <Header />
-      <Container>
-        <section className={styles.home}>
-          <div className={styles.apresentacao}>
-            <p>Olá, sou <br />
-              <span>Denis Capelete</span> <br />
-              Dev Full Stack
-            </p>
-            <Link to="/sobre" className={`${styles.btn} ${styles.btn_red}`}>
-              Saiba mais
-            </Link>
-          </div>
-          <figure>
-            <img className={styles.img_home} src="/developer-red.svg" alt="Imagem de home" />
-          </figure>
-
-        </section>
-      </Container>
-      <Footer />
+      <section className={styles.home}>
+        <div className={styles.apresentacao}>
+          <p>Olá, sou <br />
+            <span>Denis Capelete</span> <br />
+            Dev Full Stack
+          </p>
+          <Link to="/sobre" className={`${styles.btn} ${styles.btn_red}`}>
+            Saiba mais
+          </Link>
+        </div>
+        <figure>
+          <img className={styles.img_home} src="/developer-red.svg" alt="Imagem de home" />
+        </figure>
+      </section>
     </>
   )
 }
