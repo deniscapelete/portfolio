13887cca3cfda8aaded3444200eb3a40e226f4a0
Denis <capelete.denis@hotmail.com>
2024-03-21 15:17:59 -0300

    Home page responsiveness

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,22 +3,23 @@ import styles from './Home.module.css'
 
 function Home() {
   return (
-    <>
-      <section className={styles.home}>
-        <div className={styles.apresentacao}>
-          <p>Olá, sou <br />
-            <span>Denis Capelete</span> <br />
-            Dev Full Stack
-          </p>
-          <Link to="/sobre" className={`${styles.btn} ${styles.btn_red}`}>
-            Saiba mais
-          </Link>
-        </div>
-        <figure>
-          <img className={styles.img_home} src="/developer-red.svg" alt="Imagem de home" />
-        </figure>
-      </section>
-    </>
+
+    <section className={styles.home}>
+      <div className={styles.apresentacao}>
+        <p>
+          Olá, sou <br />
+          <span>Denis Capelete</span> <br />
+          Dev Full Stack
+        </p>
+        <Link to="/sobre" className={`${styles.btn} ${styles.btn_red}`}>
+          Saiba mais
+        </Link>
+      </div>
+      <figure>
+        <img className={styles.img_home} src="/developer-red.svg" alt="Imagem de home" />
+      </figure>
+    </section>
+
   )
 }
 
